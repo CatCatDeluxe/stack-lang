3871c77c04bdb450be638822108d9b8d611f2fd0
CatCatDeluxe <ratratdeluxe@gmail.com>
2026-01-19 14:27:52 -0500

    add some simple bytecode optimization

--- src/mod/compiler/optimizer.zig
+++ src/mod/compiler/optimizer.zig
@@ -1,7 +1,32 @@
 const Instruction = @import("instruction.zig");
 const cm = @import("compiler.zig");
+const Func = @import("funcs/create_func.zig");
 
-pub fn optimize(env: *cm.Env, code: []Instruction) []Instruction {
-	_ = env;
-	return code;
+/// Does simple, obvious optimizations on bytecode.
+pub fn optimize(opts: cm.CompileOptions, func: *Func) void {
+	_ = opts;
+	condensePopPass(func);
+}
+
+/// Replaces instances of add_local + pop with take_local
+fn condensePopPass(func: *Func) void {
+	var iter = func.items();
+	while (iter.next()) |i| {
+		switch (i) {
+			.instruction => |p| {
+				if (p.data != .add_local) continue;
+			},
+			else => continue,
+		}
+
+		const next_inst = switch (iter.instructionPeek(1) orelse continue) {
+			.instruction => |p| p,
+			else => continue,
+		};
+
+		if (next_inst.data == .pop) {
+			_ = func.remove(iter.index);
+			next_inst.data = .{.take_local = {}};
+		}
+	}
 }
